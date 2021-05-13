09d01124b2ce04fda180ea8466c1ff0ff36ab660
ITWebwangxin <2677855779@qq.com>
2021-05-14 00:41:17 +0800

    重新开始看

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,3 @@
-/*
- * @Description:
- * @Autor: wangxin
- * @Date: 2020-05-28 12:49:58
- * @LastEditors: Seven
- * @LastEditTime: 2020-05-30 23:57:28
- */
 module.exports = {
   presets: ['@vue/cli-plugin-babel/preset'],
   plugins: ['@babel/plugin-syntax-dynamic-import']
