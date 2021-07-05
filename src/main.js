535ae96e43ad830ea337a328f4585a38a9f15d67
ITWebwangxin <2677855779@qq.com>
2021-07-05 22:48:35 +0800

    网络提示优化

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import store from './store'
 import fastclick from 'fastclick'
 import VueLazyload from 'vue-lazyload'
 import './assets/css/base.scss' // 导入全局 css 样式
+import Loading from './plugin/loading/index'
+// 注意点: 如果想通过use的方式来注册组件, 那么必须先将组件封装成插件
+Vue.use(Loading, {
+  title: '正在加载...'
+})
 
 // 注册懒加载事件
 Vue.use(VueLazyload, {
